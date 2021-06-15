c0d96839d0b0d41099ce2047188b2848c43a44b8
Will Reynolds <goo_egg@outlook.com>
2021-06-15 12:50:05 -0500

    third commit

--- src/src/bitmap.cpp
+++ src/src/bitmap.cpp
@@ -9,23 +9,28 @@ using namespace std;
 
 namespace bitmapNS {
 
+/* Bitmap::Bitmap constructor notes
+
+m_pPixels is going to be used to create a "memmory block" that represents the whole screen (in memmory)
+The "new" operator does both the allocation and the initialization, whereas without it, uint8_t() would be initialized but not allocated
+'{}' initalizes the memory of uint8_t[width*height*3] to all zeros for each value (included to minimize bugs and set a clean initialization)
+*/
+
 Bitmap::Bitmap(int width, int height): m_width(width), m_height(height), m_pPixels(new uint8_t[width*height*3]{}) {
-    /* Bitmap::Bitmap constructor notes
-    m_pPixels is going to be used to create a "memmory block" that represents the whole screen (in memmory)
-    The "new" operator does both the allocation and the initialization, whereas without it, uint8_t() would be initialized but not allocated
-    '{}' initalizes the memory of uint8_t[width*height*3] to all zeros for each value (included to minimize bugs and set a clean initialization)*/
 
 }
 
+/* Bitmap::write constructor notes
+
+This constructor will write "bitmapFileHeader.h", "bitmapInfoHeader.h", and all bitmap data to a file
+
+first arg in .write() must be a char star ptr because it has to know where to look in memory in order to write the file
+second arg needs to say how many bytes we are going to write
+
+ios::out "|" means flag, as ios::binary (might be default in ofstream but idfk)
+*/
+
 bool Bitmap::write(string filename) {
-    /* Bitmap::write constructor notes
-
-    This constructor will write "bitmapFileHeader.h", "bitmapInfoHeader.h", and all bitmap data to a file
-
-    first arg in .write() must be a char star ptr because it has to know where to look in memory in order to write the file
-    second arg needs to say how many bytes we are going to write
-
-    ios::out "|" means flag, as ios::binary (might be default in ofstream but idfk) */
 
     bitmapFileHeader fileHeader;
     bitmapInfoHeader infoHeader;
@@ -57,30 +62,33 @@ bool Bitmap::write(string filename) {
     
 }
 
+/* setPixel notes
+    
+int x, int y ... used to store cordinates of a pixel
+
+uint8_t red, uint8_t green, uint8_t blue ... 1 byte of information initialized fom red, green, blue of that particular pixel (as specified by x & y)
+
+*/
+
 void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
-    /* setPixel notes
     
-    int x, int y ... used to store cordinates of a pixel
+    uint8_t *pPixel = m_pPixels.get(); 
 
-    uint8_t red, uint8_t green, uint8_t blue ... 1 byte of information initialized fom red, green, blue of that particular pixel (as specified by x & y)
-
-    */
-
-   uint8_t *pPixel = m_pPixels.get(); 
-   /* pPixel notes... 
-
-    The *pPixel pointer has to use .get() (method of unique ptr) in order to access underlying memeory managed by m_pPixels (declared in Bitmap::bitmap 's methods)
-        Unique pointers require .get() to access the smart pointer's memmory
-
-    We need this because we want to be able to move *pPixel to point to the correct pixel *using pointer arithmetic...
-        https://www.tutorialspoint.com/cplusplus/cpp_pointer_arithmatic.htm
-   
-    
-
-
-   */
+    pPixel += (y*3)*m_width+(x*3);  // pointer that points to a particular pixel in memmory defined by: (int x, int y)
+    pPixel[0] = blue;   // using the array subscript operators to reference the RGB byte following which pixel is actually being pointed at from above ^
+    pPixel[1] = green;  // these are in reverse order because bitmap files follow the little endian file format
+    pPixel[2] = red;
 
 }
+
+/* pPixel notes... 
+The *pPixel pointer has to use .get() (method of unique ptr) in order to access underlying memeory managed by m_pPixels (declared in Bitmap::bitmap 's methods)
+    Unique pointers require .get() to access the smart pointer's memmory
+
+we need this because we want to be able to move *pPixel to point to the correct pixel *using pointer arithmetic...
+    https://www.tutorialspoint.com/cplusplus/cpp_pointer_arithmatic.htm
+*/
+
 Bitmap::~Bitmap() {
     // destrcutor (probably wont use)
 }
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -9,8 +9,11 @@ using namespace bitmapNS;
 int main()
 {
 	//  create an object of class Bitmap, declared in bitmap.h (java style declaration where object is the same name as class but lowercase eg.. Bitmap bitmap())
+	int const WIDTH = 800;	// uppercase because const 
+	int const HEIGHT = 600;	// reason for these two is it saves us from having to re write the value over and over which is bad practice
+	Bitmap bitmap(WIDTH, HEIGHT); 
 
-	Bitmap bitmap(800, 600); // standard resolution
+	bitmap.setPixel(WIDTH/2, HEIGHT/2, 255, 255, 255);	// sets white pixel in the middle of bitmap file (each of the RGB color values range from 0-255 which is what you can fit in an ussigned byte type)
 	bitmap.write("writeBitmapFileTest.bmp"); 
 
 	cout << "finished" << endl;
